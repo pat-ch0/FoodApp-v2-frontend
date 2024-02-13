3261f889d7bd86b3322a21b25a732c73fbe8d0c1
Ismail BASSAOUD <ismail.bassaoud@gmail.com>
2024-02-13 14:12:14 +0100

    :sparkls: Add capacitor android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'frontend',
+  appName: 'T1 Food App',
   webDir: 'www',
   server: {
     androidScheme: 'https'
