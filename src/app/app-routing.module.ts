043bde47bccaf2c4611a681796c9b508d7e8e0c4
Mehdi Bouguern <mehdichevillon71@gmail.com>
2024-02-15 15:01:34 +0100

    :art: add-product-detail-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { ProductDetailComponent } from './pages/product/product-detail/product-detail.component';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'product-detail',
+    component:ProductDetailComponent,
   }
 ];
 @NgModule({
---
+++ src/app/pages/page.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PageRoutingModule } from './page-routing.module';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+
+
+@NgModule({
+  declarations: [
+    ProductDetailComponent
+  ],
+  imports: [
+    CommonModule,
+    PageRoutingModule,
+    
+  ],
+  exports:
+  [
+    ProductDetailComponent
+  ]
+})
+export class PageModule { }
