662a72a420faaf88bcd32bb8de8319090c43d6f4
IBassaoud <105388193+IBassaoud@users.noreply.github.com>
2024-02-23 17:12:46 +0100

    :sparkles: Add menu navigation (#14)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { ProductDetailComponent } from '../pages/product/product-detail/product-detail.component';
 import { SlidingComponent } from './components/sliding/sliding.component';
-import { LanguagePickerComponent } from './language-picker/language-picker.component';
+import { LanguagePickerComponent } from './components/language-picker/language-picker.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { HeaderComponent } from './components/header/header.component';
--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TabsPageRoutingModule } from './tabs-routing.module';
 import { TabsPage } from './tabs.page';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    TabsPageRoutingModule
+    TabsPageRoutingModule,
+    RouterModule
   ],
   declarations: [TabsPage]
 })
