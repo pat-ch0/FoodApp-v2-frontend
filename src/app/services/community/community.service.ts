2395aa3e6714a957c99388f21188012be971ab46
Anthony <s35391@stagiaire.aelion.fr>
2024-02-27 16:12:23 +0100

    Continue with addCommunity and add edit and delete prototypes

--- src/app/services/community/community.service.ts
+++ src/app/services/community/community.service.ts
@@ -12,4 +12,12 @@ export class CommunityService {
     async addCommunity(id: string): Promise<any> {
         return this.apiService.get(`${environment.config.endpoint.community}${id}`)
     }
+
+    async deleteCommunity(id: string): Promise<any> {
+        return this.apiService.delete(`${environment.config.endpoint.community}${id}`)
+    }
+
+    async editCommunity(id: string, newName: string): Promise<any> {
+        return this.apiService.patch(`${environment.config.endpoint.community}${id}`, newName)
+    }
 }
--- src/app/tabs/community-tab/community-tab.page.ts
+++ src/app/tabs/community-tab/community-tab.page.ts
@@ -12,8 +12,6 @@ import { HandleError } from '@service/errors/handle-error.service'
   styleUrls: ['./community-tab.page.scss'],
 })
 export class CommunityTabPage implements OnInit {
-
-
   constructor(
     private route: Router,
     private modalCreator: ModalCreator,
@@ -24,14 +22,36 @@ export class CommunityTabPage implements OnInit {
   communities: Community[] = []
 
   onDeleteCommunity(community: Community) {
-
   }
 
+  // A verifier
   async addCommunity(community: Community) {
-    console.log(community)
     try {
-      const response = await this.communityService.addCommunity(community.id);
-      this.communities.push(response.data);
+      const response = await this.communityService.addCommunity(community.id)
+      this.communities[this.communities.length] = response.data
+    }
+    catch (error: any) {
+      this.handleError.handleError(error, 'COMMUNITY_PAGE', 'COMMUNITY_NOT_FOUND', 'ERROR_FETCHING_INFOS')
+    }
+  }
+
+  // A finir
+  async deleteCommunity(community: Community) {
+    try {
+      const response = await this.communityService.deleteCommunity(community.id)
+      console.log(response.data)
+    }
+    catch (error: any) {
+      this.handleError.handleError(error, 'COMMUNITY_PAGE', 'COMMUNITY_NOT_FOUND', 'ERROR_FETCHING_INFOS')
+    }
+  }
+
+
+  // A finir
+  async editCommunity(community: Community, newName: string) {
+    try {
+      const response = await this.communityService.editCommunity(community.id, newName)
+      console.log(response.data)
     }
     catch (error: any) {
       this.handleError.handleError(error, 'COMMUNITY_PAGE', 'COMMUNITY_NOT_FOUND', 'ERROR_FETCHING_INFOS')
@@ -40,6 +60,18 @@ export class CommunityTabPage implements OnInit {
 
   async presentModal() {
     await this.modalCreator.createFormModal(this.addCommunity.bind(this), [
+      {
+        name: 'name',
+        label: 'New name',
+        type: 'text',
+        validators: [Validators.required]
+      }
+    ]);
+  }
+
+  // A corriger, pas sûr des arguments
+  async presentModalEdit() {
+    await this.modalCreator.createFormModal(this.editCommunity.bind(this, this.communities[this.communities.length]), [
       {
         name: 'name',
         label: 'Name',
@@ -54,7 +86,6 @@ export class CommunityTabPage implements OnInit {
   }
 
   ngOnInit() {
-    console.log(this.handleError)
     this.communities.push({
       id: '1',
       name: 'Community 1',
