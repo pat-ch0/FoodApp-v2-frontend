aefc5bb27e12f1f374a49398e44ede89ed5aab21
Mehdi Bouguern <mehdichevillon71@gmail.com>
2024-03-01 16:02:40 +0100

    UST - Translation -

--- src/app/pages/profil/profil.component.ts
+++ src/app/pages/profil/profil.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '@service/auth.service';
+import { UserService } from '@service/user.service';
 
 @Component({
   selector: 'app-profil',
@@ -6,12 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profil.component.scss'],
 })
 export class ProfilComponent  implements OnInit {
+  
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private userService: UserService,
+    private formBuilder: FormBuilder
+  ) { 
+    // this.loginForm = this.formBuilder.group({
+    //   email: ['', [Validators.required, Validators.email]],
+    //   password: ['', [Validators.required, Validators.minLength(6)]]
+    // });
+  }
 
-  constructor() { }
+  ngOnInit() {
+  }
 
-  ngOnInit() {}
+  // loginForm: FormGroup;
 
   alertButtons = ['Yes','No'];
   factorVerify = false
+  changeSelect: boolean = false
+  userDetails?: UserService
 
+  getUserDetails(userDetails: UserService) {
+
+    this.getUserDetails
+    console.log(userDetails);
+    return userDetails
+}
+  
 }
