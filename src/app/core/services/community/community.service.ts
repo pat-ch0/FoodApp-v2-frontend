7520b50b2ac7df538bfa4cac53b39c0ea36bd530
Azzary <43534730+Azzary@users.noreply.github.com>
2024-03-08 11:20:25 +0100

    Add documentation (#27)

--- src/app/core/services/community/community.service.ts
+++ src/app/core/services/community/community.service.ts
@@ -10,6 +10,13 @@ import { City } from '@Type/community/city.type';
 export class CommunityService {
   constructor(private apiService: ApiService) {}
 
+  /**
+   * Adds a user to a community.
+   * @param email - The email of the user.
+   * @param communityId - The ID of the community.
+   * @returns The response data.
+   * @throws Error if there is an error fetching cities.
+   */
   async addUserToCommunity(email: string, communityId: string) {
     const response = await this.apiService.post(
       `${environment.config.endpoint.community.community}/addUser`,
@@ -22,6 +29,11 @@ export class CommunityService {
     return response.data;
   }
 
+  /**
+   * Retrieves a list of cities.
+   * @returns A promise that resolves to an array of City objects.
+   * @throws Error if there is an error fetching cities.
+   */
   async getCities(): Promise<City[]> {  
     const response = await this.apiService.get(
       `${environment.config.endpoint.community.city}`
@@ -32,6 +44,12 @@ export class CommunityService {
     return response.data;
   }
 
+  /**
+   * Creates a new community.
+   * @param community - The community object to create.
+   * @returns A promise that resolves to an object containing the created community and the user role label.
+   * @throws Error if there is an error creating the community.
+   */
   async createCommunity(
     community: Community
   ): Promise<{ community: Community; userRoleLabel: string }> {
@@ -45,6 +63,12 @@ export class CommunityService {
     return response.data;
   }
 
+  /**
+   * Deletes a community.
+   * @param id - The ID of the community to delete.
+   * @returns A promise that resolves to a boolean indicating whether the community was successfully deleted.
+   * @throws Error if there is an error deleting the community.
+   */
   async deleteCommunity(id: string): Promise<boolean> {
     const response = await this.apiService.delete(
       `${environment.config.endpoint.community.community}/${id}`
@@ -56,6 +80,12 @@ export class CommunityService {
     return true;
   }
 
+  /**
+   * Edits a community.
+   * @param id - The ID of the community to edit.
+   * @param newName - The new name for the community.
+   * @returns A promise that resolves to the response data.
+   */
   async editCommunity(id: string, newName: string): Promise<any> {
     return await this.apiService.patch(
       `${environment.config.endpoint.community.community}${id}`,
@@ -63,6 +93,11 @@ export class CommunityService {
     );
   }
 
+  /**
+   * Retrieves a list of communities.
+   * @returns A promise that resolves to an array of objects containing the community and the user role label.
+   * @throws Error if there is an error fetching the communities.
+   */
   async getCommunities(): Promise<
     { community: Community; userRoleLabel: string }[]
   > {
--- src/app/core/services/modal.service.ts
+++ src/app/core/services/modal.service.ts
@@ -3,12 +3,21 @@ import { ModalController } from '@ionic/angular';
 import { FormModalComponent } from '@Shared/components/form-modal/form-modal.component';
 import { FormField } from '@Type/formField.type';
 
+/**
+ * Service for creating and managing modals.
+ */
 @Injectable({
   providedIn: 'root',
 })
 export default class ModalCreator {
   constructor(private modalCtrl: ModalController) {}
 
+  /**
+   * Creates a form modal with the specified parameters.
+   * @param submitCallback The callback function to be called when the form is submitted.
+   * @param formFields The array of form fields to be displayed in the modal.
+   * @param initialValues The initial values for the form fields (optional).
+   */
   public async createFormModal(
     submitCallback: (params: any) => Promise<void>,
     formFields: FormField[],
